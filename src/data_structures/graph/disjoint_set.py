5cb067b32f19fd7cbad71a8423ff53d530b7d659
Mark Barbaric <markantonybarbaric@hotmail.com>
2023-11-23 11:46:26 +0000

    added trapping rain water implementation.

--- src/data_structures/graph/disjoint_set.py
+++ src/data_structures/graph/disjoint_set.py
@@ -23,3 +23,32 @@ class DisjointSet:
     @property
     def size(self) -> int:
         return self._size
+
+
+
+class DisjointSet2:
+    def __init__(self, size : int):
+        self._size = size
+        self._root = [i for i in range(self.size)]
+    
+    
+    def __find(self, x):
+        while x != self._root[x]:
+            x = self._root[x]
+        return x
+
+
+    def union(self, x : int, y : int):
+        rootX = self.__find(x)
+        rootY = self.__find(y)
+        
+        if rootX != rootY:
+            self._root[rootY] = rootX
+    
+    def connected(self, x : int, y : int) -> bool:
+        return self.__find(x) == self.__find(y)
+    
+    
+    @property
+    def size(self) -> int:
+        return self._size
--- tests/data_structures/test_disjoint_set.py
+++ tests/data_structures/test_disjoint_set.py
@@ -1,4 +1,4 @@
-from src.data_structures.graph.disjoint_set import DisjointSet
+from src.data_structures.graph.disjoint_set import DisjointSet, DisjointSet2
 
 
 def test_disjoint_set():
@@ -14,5 +14,16 @@ def test_disjoint_set():
     assert not disjoint_set.connected(2,4)
     
     
+def test_disjoint_set2():
+    adj = [[0,1],[1,2],[2,3],[3,4],[2,5],[5,6],[6,7]]
+    size = 8
+    disjoint_set = DisjointSet2(size=size)
+    assert disjoint_set.size == size
+    
+    for i,j in adj:
+        disjoint_set.union(i, j)
+    
+    assert disjoint_set.connected(0,1)
+    assert disjoint_set.connected(2,7)
     
     
