9c3d6b20f474f642b333d0fc2b9f319695da52a1
Mark Barbaric <markantonybarbaric@hotmail.com>
2023-11-24 11:05:52 +0000

    added directed graph cycle tests.

--- src/algos/graph/cycles.py
+++ src/algos/graph/cycles.py
@@ -1,4 +1,22 @@
 
 def directed_graph_has_cycle(graph : list[list[int]])->bool:
     
-    return False
+    colors = [0 for _ in range(len(graph))]
+    
+    def dfs(u : int, colors : list[int], graph : list[list[int]])-> bool:
+        
+        if colors[u] == 2:
+            return False
+        
+        colors[u] = 1
+        
+        for v in graph[u]:
+            if colors[u] == 1 or dfs(v, colors, graph) == True:
+                return True
+        
+        
+        colors[u] = 2
+        return False
+    
+    
+    return dfs(0, colors, graph)
---
+++ tests/algos/graph/test_cycles.py
@@ -0,0 +1,10 @@
+from src.algos.graph.cycles import directed_graph_has_cycle
+from tests.helpers import adj_to_graph
+
+def test_directed_graph_has_cycle():
+    adj = [[0,1],[1,2],[2,3],[3,0]]
+    graph = adj_to_graph(adj, 4)
+    assert directed_graph_has_cycle(graph)
+    adj = [[0,1],[1,2],[2,0],[0,2],[3,3],[2,3]]
+    graph = adj_to_graph(adj, 4)
+    assert directed_graph_has_cycle(graph)
