0f05f9c013b609c10c3ea11a6560178e625e2806
Mark Barbaric <markantonybarbaric@hotmail.com>
2024-07-31 15:23:40 +0100

    tidyup - working on solution to return squares for negative numbers.

--- src/algos/array/two_pointers/squares_of_sorted_array.py
+++ src/algos/array/two_pointers/squares_of_sorted_array.py
@@ -1,4 +1,4 @@
-def return_squares(l: list[int]) -> list[int]:
+def return_squares(l1: list[int]) -> list[int]:
     """_summary_
 
     Args:
@@ -7,7 +7,56 @@ def return_squares(l: list[int]) -> list[int]:
     Returns:
         list[int]: _description_
     """
-    # use l and r pointers
+    # three scenarios
+    # empty
+    # only negative
+    # only positive
+    # positive and negative - special case
     
-    # if r > abs l
-    return list(0 for _ in range(len(l)))
+    ## empty
+    
+    if len(l1) == 0:
+        return []
+    
+    ## only positive
+    if l1[0] >= 0:
+        return [i ** 2 for i in l1]
+
+    i = 0
+    ans = [-1 for _ in range(len(l1))]
+    
+    l = 0
+    
+    while l < len(l1) and l1[l] < 0:
+        l += 1
+    
+    # iterate back
+    l -= 1
+    r = l + 1
+    
+    while i < len(ans):
+        negative_num = None if l < 0 else l1[l]
+        positive_num = None if r >= len(l1) else l1[r]
+        
+        if not negative_num:
+            ans[i] = positive_num ** 2
+            r += 1
+        elif not positive_num:
+            ans[i] = negative_num ** 2
+            l -= 1
+        elif positive_num and negative_num:
+            if abs(positive_num) >= abs(negative_num):
+                ans[i] = positive_num ** 2
+                r += 1
+            else:
+                ans[i] = negative_num ** 2
+                l -= 1
+        
+        #ans[i] = 
+        # l-=1
+        
+        #ans[i]
+        # r += 1
+        i += 1
+
+    return ans
--- tests/algos/array/two_pointers/test_squares_of_sorted_array.py
+++ tests/algos/array/two_pointers/test_squares_of_sorted_array.py
@@ -2,7 +2,27 @@ from src.algos.array.two_pointers import return_squares
 from tests.test_helpers import are_lists_equal
 
 
-def test_return_squares_1():
+def test_empty_list():
+    nums = []
+    ans = return_squares(nums)
+    assert ans == []
+
+
+def test_only_positive1():
+    nums = [0, 2, 3]
+    ans = return_squares(nums)
+    expected_ans = [0, 4, 9]
+    assert (are_lists_equal(expected_ans, ans, sort_lists=False))
+
+
+def test_only_negative():
+    nums = [-5, -4, -3]
+    ans = return_squares(nums)
+    expected_ans = [9, 16, 25]
+    assert (are_lists_equal(ans, expected_ans, sort_lists=False))
+    
+
+def test_with_negative():
     nums = [-4, -1, 0, 3, 10]
     ans = return_squares(nums)
     assert (are_lists_equal(nums, ans))
