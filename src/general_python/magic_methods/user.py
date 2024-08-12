da05237f9fb5eaabed29f498d0752573d7c80a44
Mark Barbaric <markantonybarbaric@hotmail.com>
2024-08-12 10:21:52 +0100

    interview_questions - more magic method implementation.

--- src/general_python/magic_methods/user.py
+++ src/general_python/magic_methods/user.py
@@ -3,5 +3,8 @@ class User:
         self._name = name
         self._age = age
 
+    def __str__(self) -> str:
+        return f"Name: {self._name}, Age: {self._age}"
+
     def __eq__(self, rhs):
         return self._name == rhs._name and self._age == rhs._age
--- tests/general_python/magic_methods/test_user.py
+++ tests/general_python/magic_methods/test_user.py
@@ -11,3 +11,9 @@ def test_not_equal_user():
     user1 = User(name="mark", age=22)
     user2 = User(name="marc", age=22)
     assert user1 != user2
+
+
+def test_user_str():
+    user1 = User(name="john", age=32)
+    user_as_str = str(user1)
+    assert user_as_str == "Name: john, Age: 32"
