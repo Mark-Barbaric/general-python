9a678cbcc505190a96b632266ddde1d92c438150
Mark Barbaric <markantonybarbaric@hotmail.com>
2024-09-04 15:01:24 +0100

    mysql - working on more complicated join to blog posts.

--- src/system_design/rest/flask/flask_sqlalchemy/app/resources/blog_posts.py
+++ src/system_design/rest/flask/flask_sqlalchemy/app/resources/blog_posts.py
@@ -1,8 +1,9 @@
 from flask_restful import Resource
-from sqlalchemy.orm import joinedload
+from sqlalchemy.orm import joinedload, aliased
 from sqlalchemy.orm.session import Session
 from ..model.blog_posts import BlogPosts
 from ..model.users import Users
+from ..model.review import Review
 
 
 class BlogPostListResource(Resource):
@@ -12,8 +13,13 @@ class BlogPostListResource(Resource):
     
     def get(self):
         session: Session = self._db_instance.session
-        blog_posts = session.query(BlogPosts).join(Users).options(joinedload(BlogPosts.user))
-        return [blog_post.to_dict() for blog_post in blog_posts]
+        user_alias = aliased(Users)
+        post_alias = aliased(BlogPosts)
+        review_alias = alised(Review)
+        query_results = (
+            session.query(BlogPosts).join(Users).options(joinedload(BlogPosts.user))
+        )
+        return [result.to_dict() for result in query_results]
 
 
 class BlogPostResource(Resource):
