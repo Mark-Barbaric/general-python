0797c76c363f9c55944c691980fd78037eaa357b
Mark Barbaric <markantonybarbaric@hotmail.com>
2024-09-18 09:13:09 +0100

    generics - working on integrating pylance for type checking.

---
+++ tests/general_python/generics/test_numeric_stack.py
@@ -0,0 +1,29 @@
+from src.general_python.generics.numeric_stack import Stack, NumericStack
+
+
+def test_str_stack():
+    stack = Stack[str]()
+    stack.push("hello")
+    assert not stack.is_empty()
+    top = stack.top()
+    assert top == "hello"
+    stack.push("my")
+    stack.push("name")
+    top = stack.top()
+    assert top == "name"
+    popped = stack.pop()
+    assert popped == "name"
+    top = stack.top()
+    assert top == "my"
+
+
+def test_int_stack():
+    stack = NumericStack[int]()
+    word: str = "hello"
+    stack.push(word)
+    stack.push(1)
+    assert not stack.is_empty()
+    assert stack.top() == 1
+    popped = stack.pop()
+    assert popped == 1
+    assert stack.is_empty()
